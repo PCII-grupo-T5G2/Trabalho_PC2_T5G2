5acc842327eba3e07a81276b1d78435a5fa3a45a
joaorocha0503 <161719802+joaorocha0503@users.noreply.github.com>
2024-05-23 15:28:30 +0100

    Update prato.py

--- classes/prato.py
+++ classes/prato.py
@@ -9,21 +9,20 @@ class Prato(Gclass):
     nkey = 1
     
     # class attributes, identifier attribute must be the first one on the list
-    att = ['_cod','_semana', '_carne','_peixe',"_vegetariano"]
+    att = ['_cod','_semana','_nome', '_tipo']
     # Class header title
     header = 'Prato'
     # field description for use in, for example, in input form
-    des = ['Cod','Semana', 'Carne','Peixe','Vegetariano']
+    des = ['Cod','Semana', 'Nome','Tipo']
     # Constructor: Called when an object is instantiated
     
 
-    def __init__(self, cod,semana,carne,peixe,vegetariano):
+    def __init__(self, cod,semana,nome,tipo):
         super().__init__()
         self._cod = cod
         self._semana = semana
-        self._carne = carne
-        self._peixe = peixe
-        self._vegetariano = vegetariano
+        self._nome = nome
+        self._tipo = tipo
         
         Prato.obj[cod] = self
 
@@ -39,16 +38,18 @@ class Prato(Gclass):
         return self._semana
     
     @property
-    def carne(self):
-        return self._carne
+    def nome(self):
+        return self._nome
     
     @property
-    def peixe(self):
-        return self._peixe
+    def tipo(self):
+        return self._tipo
     
-    @property
-    def vegetariano(self):
-        return self._vegetariano
+    def procuraNovoCodigo():
+        maior_codigo = 0
+        if len(Prato.lst) > 0:
+            return int(Prato.lst[-1])
+        return maior_codigo
     
 
     
